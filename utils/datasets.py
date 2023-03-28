ce320fbe419fa18a9449d9d53894100f7dcb7006
nelioasousa <corneliosousa@alu.ufc.br>
2023-03-28 14:37:06 -0300

    LoadImages class refactoring

--- utils/datasets.py
+++ utils/datasets.py
@@ -147,10 +147,11 @@ class LoadImages:  # for inference
         self.nf = ni + nv  # number of files
         self.video_flag = [False] * ni + [True] * nv
         self.mode = 'image'
-        if any(videos):
-            self.new_video(videos[0])  # new video
-        else:
-            self.cap = None
+        self.cap = None
+        # if any(videos):
+        #     self.new_video(videos[0])  # new video
+        # else:
+        #     self.cap = None
         assert self.nf > 0, f'No images or videos found in {p}. ' \
                             f'Supported formats are:\nimages: {img_formats}\nvideos: {vid_formats}'
 
@@ -161,22 +162,23 @@ class LoadImages:  # for inference
     def __next__(self):
         if self.count == self.nf:
             raise StopIteration
+        
         path = self.files[self.count]
-
         if self.video_flag[self.count]:
             # Read video
             self.mode = 'video'
-            ret_val, img0 = self.cap.read()
-            if not ret_val:
-                self.count += 1
+            try:
+                ret_val, img0 = self.cap.read()
+            except AttributeError:
+                self.new_video(path)
+                ret_val, img0 = self.cap.read()
+            while not ret_val:
                 self.cap.release()
+                self.count += 1
                 if self.count == self.nf:  # last video
                     raise StopIteration
-                else:
-                    path = self.files[self.count]
-                    self.new_video(path)
-                    ret_val, img0 = self.cap.read()
-
+                self.new_video(self.files[self.count])
+                ret_val, img0 = self.cap.read()
             self.frame += 1
             print(f'video {self.count + 1}/{self.nf} ({self.frame}/{self.nframes}) {path}: ', end='')
 
@@ -189,9 +191,10 @@ class LoadImages:  # for inference
 
         # Padded resize
         img = letterbox(img0, self.img_size, stride=self.stride)[0]
-
+        
         # Convert
         img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = img[None, :, :, :]
         img = np.ascontiguousarray(img)
 
         return path, img, img0, self.cap
